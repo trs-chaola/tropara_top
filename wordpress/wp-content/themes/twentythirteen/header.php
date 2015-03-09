344dd0b8a7bdf2a36f325db2865653570adc65d6
trd <trd@192.168.1.126>
2015-03-09 10:00:02 +0800

    fix header

--- wordpress/wp-content/themes/twentythirteen/header.php
+++ wordpress/wp-content/themes/twentythirteen/header.php
@@ -22,6 +22,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/header.css" type="text/css">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
@@ -33,19 +34,16 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</a>
+		<div id="head" style="background-image: url(<?php bloginfo('template_url'); ?>/images/tropara_img/headers/header_bg.png)">
+			<div class="table">
+				<ul id="header-images">
+					<li><image id="h_image" src="<?php bloginfo('template_url'); ?>/images/tropara_img/headers/tropara_logo.png"></image></li>
+					<li><image id="h_image" src="<?php bloginfo('template_url'); ?>/images/tropara_img/headers/fish_icon.png"></image></li>
+					<li><image id="h_image3" src="<?php bloginfo('template_url'); ?>/images/tropara_img/headers/header_bt.png"></image></li>		
+				</ul>
+			</div>
+		</div>
 
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
 		</header><!-- #masthead -->
 
 		<div id="main" class="site-main">
